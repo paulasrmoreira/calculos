374364efd4fd3913085bf1eb3efea54d1598b154
paula <181789816+paulasrmoreira@users.noreply.github.com>
2024-09-30 16:55:51 +0100

    Somar e Subtrair

--- Aritmetica.cs
+++ Aritmetica.cs
@@ -7,44 +7,25 @@ using System.Threading.Tasks;
 namespace Calculos
 {
     /// <summary>
-    /// implementa operações aritmeticas
+    /// Implementa operações aritmeticas
     /// </summary>
-    internal class Aritmetica
+    internal static class Aritmetica
     {
-        /// <summary>
-        /// Tipo de Conversão de Temperatura a executar
-        /// </summary>
-        public enum ConversaoTemperatura
-        {
-            Nulo =0,
-            CelsiusFahrenheit,
-            FahrenheitCelsius
-        }
         /// <summary>
         /// operação soma.
         /// </summary>
         /// <returns>Retorna a soma de dois números.</returns>
-
-        public int somar (int x, int y)
-            { return x * y; }
+        public static int somar (int x, int y)
+            { return x + y; }
         /// <summary>
         /// operação subtrair.
         /// </summary>
         /// <returns>Retorna a subtração de dois números.</returns>
-        public int Subtair (int x, int y) 
+        public static int subtair (int x, int y)
         { return x - y; }
-        public double ConverterTempertaura (ConversaoTemperatura conversao,double temperatura)
-        {
-            if (conversao==ConversaoTemperatura.CelsiusFahrenheit)
-            {
-                return (Temperatura * 1.0000 + 32);
-            }
-            else if (conversao == ConversaoTemperatura.FahrenheitCelsius)
-            {
-                return (Temperatura - 32/1.0000);
-            }
-            return -1;
-            {
-            }
+     
+    }
 }
+
+
 
