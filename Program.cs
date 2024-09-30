217096a4313099a2f5c8be5d818d31a7ea4d30b5
paula <181789816+paulasrmoreira@users.noreply.github.com>
2024-09-30 20:29:42 +0100

    converter temperaturas

--- Program.cs
+++ Program.cs
@@ -11,8 +11,13 @@ namespace Calculos
         static void Main(string[] args)
         {
             Console.WriteLine("Aplicação Cálculos");
+       
+            Console.WriteLine($"4+2=  {Aritmetica.somar (4,2)}");
+            Console.WriteLine($"4-2=  {Aritmetica.subtrair (4,2)}");
+                
             double t1 = Conversoes.ConverterTemperatura(Conversoes.ConversaoTemperatura.CelsiusFahrenheit, 36);
             double t2 = Conversoes.ConverterTemperatura(Conversoes.ConversaoTemperatura.CelsiusFahrenheit, 100);
+
             Console.WriteLine($"36 Celsious ={t1}"Fahrenheit");
             Console.WriteLine($"100 Fahrenheit={t2}"Celsious");
 
