3fbbddb0a4039ff9af8dbc2567c9fb83d3ea8bfd
paula <181789816+paulasrmoreira@users.noreply.github.com>
2024-09-30 11:28:06 +0100

    Criar a Classe estática

--- Aritmetica.cs
+++ Aritmetica.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace Calculos
 {
+    /// <summary>
+    /// implementa operações aritmeticas
+    /// </summary>
     internal class Aritmetica
     {
-    }
+        /// <summary>
+        /// Tipo de Conversão de Temperatura a executar
+        /// </summary>
+        public enum ConversaoTemperatura
+        {
+            Nulo =0,
+            CelsiusFahrenheit,
+            FahrenheitCelsius
+        }
+        /// <summary>
+        /// operação soma.
+        /// </summary>
+        /// <returns>Retorna a soma de dois números.</returns>
+
+        public int somar (int x, int y)
+            { return x * y; }
+        /// <summary>
+        /// operação subtrair.
+        /// </summary>
+        /// <returns>Retorna a subtração de dois números.</returns>
+        public int Subtair (int x, int y) 
+        { return x - y; }
+        public double ConverterTempertaura (ConversaoTemperatura conversao,double temperatura)
+        {
+            if (conversao==ConversaoTemperatura.CelsiusFahrenheit)
+            {
+                return (Temperatura * 1.0000 + 32);
+            }
+            else if (conversao == ConversaoTemperatura.FahrenheitCelsius)
+            {
+                return (Temperatura - 32/1.0000);
+            }
+            return -1;
+            {
+            }
 }
+
