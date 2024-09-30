e55dfaf2fdc11650fcaf2e6b65b7cd247e9dc44f
paula <181789816+paulasrmoreira@users.noreply.github.com>
2024-09-30 16:41:50 +0100

    Criar variável static

--- Conversoes.cs
+++ Conversoes.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace Calculos
 {
-    internal class Conversoes
-    {
+    /// <summary>
+    /// Implementa operações de conversão.
+    /// </summary>
+    internal static class Conversoes
+        
+    {   /// <summary>
+        /// Tipo de Conversão de Temperatura a executar
+        /// </summary>
+        public enum ConversaoTemperatura
+        {
+            Nulo = 0,
+            CelsiusFahrenheit,
+            FahrenheitCelsius
+        }
+        /// <summary>
+        /// Conversão de temperaturas.
+        /// </summary>
+        /// <param name="conversao"> A conversão a efetuar </param>
+        /// <param name="temperatura"> A temperatura a converter</param>
+        /// <returns>
+        /// Retorna o resultado da conversão.</returns>
+        public static double ConverterTempertaura(ConversaoTemperatura conversao, double temperatura)
+        {
+            if (conversao == ConversaoTemperatura.CelsiusFahrenheit)
+            {
+                return (temperatura * 1.0000 + 32);
+            }
+            else if (conversao == ConversaoTemperatura.FahrenheitCelsius)
+            {
+                return (temperatura - 32 / 1.0000);
+            }
+            return -1;
+        
+        }
     }
 }
